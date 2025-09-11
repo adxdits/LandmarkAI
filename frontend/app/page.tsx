a3e6df233e781e0490b8f0864a37b85ada1fab13
adxdits <adamalmounayar@hotmail.com>
2025-09-11 18:39:29 +0200

    frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import ButtonComponent from "../app/components/ButtonComponent/ButtonComponent"
+
+export default function Home() {
+  return (
+    <><div>One Euro Trip</div><ButtonComponent/></>
+  )
+}
